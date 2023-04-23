f5d1ab65e6779e184cada6e07872acda5031ffa7
Dennis Kimani <dxaldovah@gmail.com>
2023-04-23 12:36:38 -0700

    shell_01

--- Simple_shell_0.1.c
+++ Simple_shell_0.1.c
@@ -22,14 +22,9 @@ int main(void)
 
 	while (1)
 	{
-		printf(PROMPT);
+		write(STDOUT_FILENO, PROMPT, 1);
 		fflush(stdout);
-<<<<<<< HEAD:shell.c
-
-		if (fgets(input, MAX_INPUT, stdin) == NULL)
-=======
 		if (getline(&input, &input_size, stdin) == -1)
->>>>>>> cd8e2033817997627548c4f97d73ccfe9b0c2fc2:Simple_shell_0.1.c
 		{
 			write(STDOUT_FILENO, "\n", 1);
 			free(input);
@@ -37,30 +32,17 @@ int main(void)
 		}
 		input[strcspn(input, "\n")] = '\0';
 		pid = fork();
-<<<<<<< HEAD:shell.c
-
-=======
->>>>>>> cd8e2033817997627548c4f97d73ccfe9b0c2fc2:Simple_shell_0.1.c
 		if (pid < 0)
 		{
 			perror("fork");
 			exit(EXIT_FAILURE);
 		}
-<<<<<<< HEAD:shell.c
-
-		if (pid == 0)
-=======
 		else if (pid == 0)
->>>>>>> cd8e2033817997627548c4f97d73ccfe9b0c2fc2:Simple_shell_0.1.c
 		{
 			char *args[2] = { NULL };
 
 			args[0] = input;
-<<<<<<< HEAD:shell.c
-			if (execve(input, args, environ) < 0)
-=======
 			if (execve(args[0], args, NULL) < 0)
->>>>>>> cd8e2033817997627548c4f97d73ccfe9b0c2fc2:Simple_shell_0.1.c
 			{
 				perror("execve");
 				exit(EXIT_FAILURE);
