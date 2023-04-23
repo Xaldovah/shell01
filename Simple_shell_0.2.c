d21b2745d10a822018801fc0e2641623d693eb34
Nomthandaso <deanerysstormbornmabu@gmail.com>
2023-04-23 15:01:17 -0700

    shell 02

--- Simple_shell_0.2.c
+++ Simple_shell_0.2.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #define PROMPT "shell$ "
 /**
@@ -13,8 +14,10 @@ int main(void)
 {
 	char *cmd = NULL, *cmd_cpy = NULL, *token = NULL;
 	char *delim = " \n";
+	pid_t pid;
 	size_t n = 0;
 	int argc = 0, i = 0;
+	int status;
 	char **argv = NULL;
 
 	while(1)
@@ -45,6 +48,25 @@ int main(void)
 			i++;
 		}
 		argv[i] = NULL;
+		
+		pid = fork();
+		if (pid == 0)
+		{
+			if (execve(argv[0], argv, NULL) = -1)
+			{
+				perror("execve");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else if (pid > 0)
+		{
+			wait(&status);
+		}
+		else
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
 
 		free(cmd);
 		free(argv);
