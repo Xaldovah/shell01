f9dde6997e906d46f1d486188c5a3c6b0828baee
Dennis Kimani <dxaldovah@gmail.com>
2023-04-26 16:45:18 -0700

    shell

--- main.c
+++ main.c
@@ -1,105 +1,128 @@
 #include "shell.h"
 
 /**
- * main - This function prints the shell 
- *
- * Return: Success.
+ * This function initializes the path array by parsing PATH environ variable.
+ * It splits the string into individual paths and stores them in the path array
  */
+void init_paths()
+{
+	char *path_var;
+	char *path_str;
+	int i = 0;
+	char *path_token;
+	
+	path_var = getenv("PATH");
+	if (path_var == NULL)
+	{
+		perror("PATH");
+		exit(EXIT_FAILURE);
+	}
+	path_str = strdup(path_var);
+	if (path_str == NULL)
+	{
+		perror("memory");
+		exit(EXIT_FAILURE);
+	}
+	path_token = strtok(path_str, ":");
+	while (path_token != NULL && i < MAX_PATHS - 1)
+	{
+		path[i++] = path_token;
+		path_token = strtok(NULL, ":");
+	}
+	path[i] = NULL;
+	free(path_str);
+}
+
+char *find_command(char *command)
+{
+	char path_buffer[MAX_PATH_LENGTH];
+	int i;
+	for (i = 0; path[i] != NULL; i++) {
+		snprintf(path_buffer, MAX_PATH_LENGTH, "%s/%s", path[i], command);
+		if (access(path_buffer, X_OK) == 0)
+		{
+			return strdup(path_buffer); /* make str cpy */
+		}
+	}
+    exit(EXIT_FAILURE);
+}
+
+void parse_input(char *input, char *args[])
+{
+	int i = 0;
+	char *token = NULL;
+	
+	token = strtok(input, " \t\n");
+	while (token != NULL && i < MAX_ARGS - 1)
+	{
+		args[i++] = token;
+		token = strtok(NULL, " \t\n");
+	}
+	args[i] = NULL; /* add a NULL pointer to mark the end of the array */
+}
 
 int main(void)
 {
-        pid_t pid;
-        char *input = NULL, *path = NULL;
-        size_t input_size = 0;
-        int status;
+	char *args[MAX_ARGS];
+	pid_t pid;
+	int status;
+	char *command;
 
-        while (1)
-        {
-                write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
-                if (getline(&input, &input_size, stdin) == -1)
-                {
-                        write(STDOUT_FILENO, "\n", 1);
-                        free(input);
-                        exit(EXIT_FAILURE);
-                }
-                input[strcspn(input, "\n")] = '\0';
+	init_paths();
 
-                // Handle exit built-in
-                if (strcmp(input, "exit") == 0)
-                {
-                        free(input);
-                        exit(EXIT_SUCCESS);
-                }
+	while (1)
+	{
+		char *input_buffer = NULL;
+		size_t buffer_size = 0;
 
-                // Handle env built-in
-                if (strcmp(input, "env") == 0)
-                {
-                        char **env = NULL;
-                        int i;
-
-                        for (i = 0; environ[i] != NULL; i++)
-                        {
-                                write(STDOUT_FILENO, environ[i], strlen(environ[i]));
-                                write(STDOUT_FILENO, "\n", 1);
-                        }
-                        continue;
-                }
-
-                // Get the PATH environment variable
-                path = _getenv("PATH");
-
-                // Tokenize the command line
-                char **args = tokenize_cmdline(input);
-
-                // Search for the command in the directories listed in PATH
-                char *dir, *cmd;
-                int found = 0;
-                while ((dir = strtok_r(path, ":", &path)) != NULL)
-                {
-                        cmd = malloc(strlen(dir) + strlen(args[0]) + 2);
-                        sprintf(cmd, "%s/%s", dir, args[0]);
-
-                        if (access(cmd, X_OK) == 0)
-                        {
-                                found = 1;
-                                break;
-                        }
-
-                        free(cmd);
-                }
-
-                // If the command wasn't found, print an error message and continue
-                if (!found)
-                {
-                        write(STDERR_FILENO, "Command not found\n", 18);
-                        free(args);
-                        continue;
-                }
-
-                // Execute the command
-                pid = fork();
-                if (pid < 0)
-                {
-                        perror("fork");
-                        exit(EXIT_FAILURE);
-                }
-                else if (pid == 0)
-                {
-                        if (execve(cmd, args, environ) < 0)
-                        {
-                                perror("execve");
-                                exit(EXIT_FAILURE);
-                        }
-                }
-                else
-                {
-                        wait(&status);
-                }
-
-                free(args);
-                free(cmd);
-        }
-
-        free(input);
-        exit(EXIT_SUCCESS);
+		write(STDOUT_FILENO, PROMPT, strlen(PROMPT) + 1);
+		fflush(stdout);
+		if (getline(&input_buffer, &buffer_size, stdin) == -1)
+		{
+			/* handle EOF or error */
+			write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
+		input_buffer[strcspn(input_buffer, "\n")] = '\0';
+		
+		parse_input(input_buffer, args);
+		
+		if (args[0] == NULL)
+		{
+			continue; /* empty input, prompt again */
+		}
+		if (strcmp(args[0], "exit") == 0)
+		{
+			break;
+		}
+		command = find_command(args[0]);
+		if (command == NULL)
+		{ /* Command doesn't exist, */
+			char *msg = "command not found\n";
+			write(STDERR_FILENO, args[0], strlen(args[0]));
+			write(STDERR_FILENO, ": ", 2);
+			write(STDERR_FILENO, msg, strlen(msg));
+			continue;
+		}
+		pid = fork();
+		if (pid < 0)
+		{
+			perror("fork");
+			free(command);
+			continue;
+		}
+		else if (pid == 0)
+		{
+			execve(command, args, environ);
+			perror("execve");
+			free(command);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			waitpid(pid, &status, 0);
+			free(command);
+		}
+	}
+	exit(EXIT_SUCCESS);
 }
--- shell.h
+++ shell.h
@@ -11,8 +11,12 @@
 
 #define MAX_INPUT 1024
 #define PROMPT "shell$ "
+#define MAX_ARGS 64
+#define MAX_PATHS 64
+#define MAX_PATH_LENGTH 256
 
 /* Function prototypes */
+char *path[MAX_PATHS + 1]; /* array to hold paths from PATH environment var*/
 extern char **environ;
 
 char *string_toupper(char *s);
@@ -27,5 +31,9 @@ size_t strlen(const char *s);
 char *strcat(char *dest, const char *src);
 char *strcpy(char *dest, const char *src);
 size_t strcspn(const char *s, const char *reject);
+
+void parse_input(char *input, char **args);
+char *find_command(char *command);
+void free_array(char **args);
 
 #endif
