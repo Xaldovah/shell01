34fe53355fa3652b1b03c73a04209c877a5d8da4
Dennis Kimani <dxaldovah@gmail.com>
2023-04-26 17:08:00 -0700

    shell

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include "shell.h"
 
 /**
- * This function initializes the path array by parsing PATH environ variable.
+ * init_paths - This function inits the path array by parsing PATH environ var.
  * It splits the string into individual paths and stores them in the path array
+ *
+ * Return: void.
  */
-void init_paths()
+void init_paths(void)
 {
 	char *path_var;
 	char *path_str;
 	int i = 0;
 	char *path_token;
-	
+
 	path_var = getenv("PATH");
 	if (path_var == NULL)
 	{
@@ -33,25 +35,42 @@ void init_paths()
 	free(path_str);
 }
 
+/**
+ * find_command - This function takes a string input (command)
+ * @command: user input string
+ *
+ * Return: a pointer to a character
+ */
+
 char *find_command(char *command)
 {
 	char path_buffer[MAX_PATH_LENGTH];
 	int i;
-	for (i = 0; path[i] != NULL; i++) {
+
+	for (i = 0; path[i] != NULL; i++)
+	{
 		snprintf(path_buffer, MAX_PATH_LENGTH, "%s/%s", path[i], command);
 		if (access(path_buffer, X_OK) == 0)
 		{
-			return strdup(path_buffer); /* make str cpy */
+			return (strdup(path_buffer)); /* make str cpy */
 		}
 	}
-    exit(EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 }
+
+/**
+ * parse_input - This function parses user input command
+ * @input: pointer to a string called input
+ * @args: pointer to an array of strings called args
+ *
+ * Return: void.
+ */
 
 void parse_input(char *input, char *args[])
 {
 	int i = 0;
 	char *token = NULL;
-	
+
 	token = strtok(input, " \t\n");
 	while (token != NULL && i < MAX_ARGS - 1)
 	{
@@ -60,6 +79,12 @@ void parse_input(char *input, char *args[])
 	}
 	args[i] = NULL; /* add a NULL pointer to mark the end of the array */
 }
+
+/**
+ * main - This function prints the shell
+ *
+ * Return: EXIT_SUCCESS
+ */
 
 int main(void)
 {
@@ -84,9 +109,9 @@ int main(void)
 			break;
 		}
 		input_buffer[strcspn(input_buffer, "\n")] = '\0';
-		
+
 		parse_input(input_buffer, args);
-		
+
 		if (args[0] == NULL)
 		{
 			continue; /* empty input, prompt again */
@@ -99,6 +124,7 @@ int main(void)
 		if (command == NULL)
 		{ /* Command doesn't exist, */
 			char *msg = "command not found\n";
+
 			write(STDERR_FILENO, args[0], strlen(args[0]));
 			write(STDERR_FILENO, ": ", 2);
 			write(STDERR_FILENO, msg, strlen(msg));
