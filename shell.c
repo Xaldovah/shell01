3ba6279a81535d953db5ee736ee0df7bfc141f84
Dennis Kimani <dxaldovah@gmail.com>
2023-04-27 21:16:32 -0700

    latest changes

--- shell.c
+++ shell.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 /**
- * main - makesa simple shell
+ * main - makes a simple shell
  *
- * Exit: (SUCCESS)
+ * Return: (0)
  */
 
 int main(void)
@@ -12,42 +12,38 @@ int main(void)
 	size_t ln_size = 0;
 	ssize_t ln_length = 0;
 
-    while (ln_length >= 0)
-    {
-        signal(SIGINT, handle_signal);
-        if (isatty(STDIN_FILENO))
-        {
-            write(STDOUT_FILENO, "shell$ ", 7);
-        }
-        ln_length = getline(&ln, &ln_size, stdin);
-        if (ln_length == -1)
-        {
-            if (isatty(STDIN_FILENO))
-            {
-                write(STDOUT_FILENO, "\n", 1);
-            }
-            break;
-        }
-        str_len = count_input(ln);
-        if (ln[0] != '\n' && str_len > 0)
-        {
-            tokens = tokenize(ln, " \t", str_len);
-            Flag = builtin(tokens, ln);
-            if (!Flag)
-            {
-                tokens[0] = find(tokens[0]);
-                if (tokens[0] && access(tokens[0], X_OK) == 0)
-                {
-                    exec(tokens[0], tokens);
-                }
-                else
-                {
-                    perror("./hsh");
-                }
-                free(tokens);
-            }
-        }
-    }
-    free(ln);
-    exit(EXIT_FAILURE);
+	while (ln_length >= 0)
+	{
+		signal(SIGINT, handle_signal);
+		if (isatty(STDIN_FILENO))
+		{
+			write(STDOUT_FILENO, PROMPT, strlen(PROMPT) + 1);
+		}
+		ln_length = getline(&ln, &ln_size, stdin);
+		if (ln_length == -1)
+		{
+			if (isatty(STDIN_FILENO))
+			{
+				write(STDOUT_FILENO, "\n", 1);
+			}
+			break;
+		}
+		str_len = count_input(ln);
+		if (ln[0] != '\n' && str_len > 0)
+		{
+			tokens = tokenize(ln, " \t", str_len);
+			Flag = builtin(tokens, ln);
+			if (!Flag)
+			{
+				tokens[0] = find(tokens[0]);
+				if (tokens[0] && access(tokens[0], X_OK) == 0)
+				{
+					exec(tokens[0], tokens);
+				}
+				free(tokens);
+			}
+		}
+	}
+	free(ln);
+	return (0);
 }
