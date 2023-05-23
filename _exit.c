c5a2dda183ba281ea447601030df7ee975f9c7f4
Dennis Kimani <dxaldovah@gmail.com>
2023-05-22 22:10:37 -0700

    exit 98

--- _exit.c
+++ _exit.c
@@ -9,30 +9,30 @@
 
 void res_handle_exit(char **res_tokens, char *res_line)
 {
-	int res_status = 0;
+    int res_status = 0;
 
-	if (res_tokens[1] == NULL || (!_strcmp(res_tokens[1], "0")))
-	{
-		free(res_tokens);
-		free(res_line);
-		exit(0);
-	}
-	res_status = _atoi(res_tokens[1]);
-	if (res_status != 0)
-	{
-		free(res_tokens);
-		free(res_line);
-		exit(res_status);
-	}
-	else
-	{
-		_puts("exit: prohibited: ");
-		_puts(res_tokens[1]);
-		_puts("\n");
-		exit(2);
-	}
+    if (res_tokens[1] == NULL || (!_strcmp(res_tokens[1], "0")))
+    {
+        free(res_tokens);
+        free(res_line);
+        exit(0);
+    }
 
-	free(res_tokens);
-	free(res_line);
-	exit(EXIT_SUCCESS);
+    res_status = atoi(res_tokens[1]);
+
+    if (res_status != 0)
+    {
+        free(res_tokens);
+        free(res_line);
+        exit(res_status);
+    }
+    else
+    {
+        _puts("exit: prohibited: ");
+        _puts(res_tokens[1]);
+        _puts("\n");
+        free(res_tokens);
+        free(res_line);
+        exit(2);
+    }
 }
